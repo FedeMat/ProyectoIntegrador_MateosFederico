4867fc530cca0fbfafed1892c31571fb0de065fb
FedeMat <federico.mateos4@gmail.com>
2022-10-30 22:57:07 -0300

    Login

--- ProyectoIntegrador_MateosFederico/FrontEnd Proyecto/src/app/app.module.ts
+++ ProyectoIntegrador_MateosFederico/FrontEnd Proyecto/src/app/app.module.ts
@@ -14,6 +14,8 @@ import { ProyectosComponent } from './components/proyectos/proyectos.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
 
 
 @NgModule({
@@ -27,7 +29,9 @@ import { HttpClient, HttpClientModule } from '@angular/common/http';
     EducacionComponent,
     SkillsComponent,
     ProyectosComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
